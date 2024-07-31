6edcb8828aff03a19b8d8b50a533c87e83484d11
sebousan <sebousan@gmail.com>
2024-07-31 13:46:12 +0200

    postcss

--- postcss.config.js
+++ postcss.config.js
@@ -8,9 +8,10 @@ module.exports = {
     '@fullhuman/postcss-purgecss': {
       mode: 'all',
       content: ['./hugo_stats.json'],
-      dynamicAttributes: ['aria-current', 'data-anim', 'href', 'role'],
+      dynamicAttributes: ['aria-current', 'href', 'role'],
       safelist: {
-        standard: ['show', 'fade', 'leaflet-tile-pane', /-backdrop$/]
+        standard: ['show', 'fade', 'leaflet-tile-pane', /-backdrop$/],
+        greedy: [/^data-anim$/]
       },
       defaultExtractor: (content) => {
         let els = JSON.parse(content).htmlElements;

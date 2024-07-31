6cbeaeaad807579b2e69de0dac12416f63693e08
sebousan <sebousan@gmail.com>
2024-07-31 10:53:25 +0200

    postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,36 +1,32 @@
 /* eslint-disable no-undef */
 module.exports = {
-    plugins: {
-        autoprefixer: {},
-        cssnano: {
-            preset: 'default'
-        },
-        '@fullhuman/postcss-purgecss': {
-            mode: 'all',
-            content: ['./hugo_stats.json'],
-            safelist: {
-                standard: [
-                    'show',
-                    'fade',
-                    /-backdrop$/,
-                    /^is-/,
-                    /^has-/,
-                    /^js-/
-                ],
-                deep: [
-                    /^tobii/
-                ],
-                greedy: [
-                    /^data-anim$/,
-                    /^aria-current$/,
-                    /^role$/
-                ]
-            },
-            defaultExtractor: (content) => {
-                let els = JSON.parse(content).htmlElements;
-                els = els.tags.concat(els.classes);
-                return els;
-            }
-        }
+  plugins: {
+    autoprefixer: {},
+    cssnano: {
+      preset: 'default'
+    },
+    '@fullhuman/postcss-purgecss': {
+      mode: 'all',
+      content: ['./hugo_stats.json'],
+      dynamicAttributes: ['aria-current', 'href', 'role'],
+      safelist: {
+        standard: [
+          'show',
+          'fade',
+          /-backdrop$/,
+          /^leaflet-/,
+          /^is-/,
+          /^has-/,
+          /^js-/,
+          /^[href^="#"]/,
+          /^[href^="mailto"]/
+        ]
+      },
+      defaultExtractor: (content) => {
+        let els = JSON.parse(content).htmlElements;
+        els = els.tags.concat(els.classes);
+        return els;
+      }
     }
+  }
 };
